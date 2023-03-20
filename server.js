42fb7255c3d2165b4478faafad76cd6ab8bbd366
Hoang Quan <21522493@gm.uit.edu.vn>
2023-03-20 14:09:19 +0700

    cai dat express

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
-const http = require('http');
+const express = require('express')
 
-const server = http.createServer((req, res) => {
-    console.log('run request ...')
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<h3>Hello world! </h3>');
-    res.write('<h2>From Venn.2866 </h2>');
-    res.end();
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World! You are handsome')
 })
 
-server.listen(3000, 'localhost', () => {
-    console.log('Node.JS server is running on port: 3000');
+app.get('/about', (req, res) => {
+    res.send('What are you thinking?')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
 })
